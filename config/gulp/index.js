c05bb37f1e697bbef7924b605ba8171dff897bbd
trongbang <trongbang86@gmail.com>
2015-10-28 14:23:16 +1100

    added documentation for gulp tasks

--- config/gulp/index.js
+++ config/gulp/index.js
@@ -7,10 +7,18 @@ var gulp 			= require('gulp'),
 	del				= require('del'),
 	gulpUtil		= require('gulp-util');
 
+/**
+This cleans out the .tmp folder
+*/
 gulp.task('clean', function(cb){
 	return del(['.tmp'], cb);
 });
 
+/**
+This compiles sass files. Then it auto-prefixes, concats and
+does all other jobs related to css
+Result is all.css to be used
+*/
 gulp.task('style', ['clean'], function(){
 	return sass('frontend/stylesheets/**.sass').
 		pipe(autoprefixer('last 2 versions')).
