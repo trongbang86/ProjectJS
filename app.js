3dd536bd154d60f8608b0b9f0e99c47d5e22e673
trongbang <trongbang86@gmail.com>
2015-10-29 16:43:25 +1100

    changed express to use html extensions and moved views folder to frontend folder

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ global.Project = require('./config/bootstrap.js')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'frontend', 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,13 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, Project.gulp.tmpFolder)));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
