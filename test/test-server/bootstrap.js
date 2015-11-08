f067371c96c815a1b843842dc4d99c4a5ac38854
trongbang86 <trongbang86@gmail.com>
2015-11-08 11:08:31 +1100

    changed shutdown hook for test-server/bootstrap.js

--- test/test-server/bootstrap.js
+++ test/test-server/bootstrap.js
@@ -1,6 +1,7 @@
 /* registering global variables as testing utilities */
 global._ 		= require('underscore');
 global.expect	= require('chai').expect;
+var ProjectJS	= require('../../config/bootstrap.js');
 
 before(function(){
 	/* loading the environment */
@@ -9,12 +10,13 @@ before(function(){
 	global.TestProject = app.Project;
 });
 
-after(function(){
-
-	/* Defining hooks when server is shutdown */
-	// listen for TERM signal .e.g. kill 
-	process.on ('SIGTERM', TestProject.shutdown);
-
-	// listen for INT signal e.g. Ctrl-C
-	process.on ('SIGINT', TestProject.shutdown); 
+/* Defining hooks when server is shutdown */
+// listen for TERM signal .e.g. kill 
+process.on ('SIGTERM', function(){
+  ProjectJS.Projects.shutdown(process.exit);
 });
+
+// listen for INT signal e.g. Ctrl-C
+process.on ('SIGINT', function(){
+  ProjectJS.Projects.shutdown(process.exit);
+}); 
