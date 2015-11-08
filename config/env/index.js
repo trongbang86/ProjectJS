38c95921a8855e8f6d6143af50c025c7b3f81525
trongbang86 <trongbang86@gmail.com>
2015-11-08 11:07:06 +1100

    added loading hidden config files for different environments

--- config/env/index.js
+++ config/env/index.js
@@ -1,7 +1,9 @@
 /*
-This loads settings from corresponding configuration json-based file
-@this: Project object - settings
-@return: [nconf]
+ * This loads settings from corresponding configuration json-based file
+ * @this: any {} which must have the attribute env.
+ * 		For Example:
+ * 			- Project object - settings
+ * @return: [nconf]
 */
 module.exports = function(){
 	var env = this.env;
@@ -16,6 +18,7 @@ module.exports = function(){
 
 	var path = require('path');
 	nconf.argv().env()
+		.file("__" + env + "__", path.join(__dirname, "__" + env + "__.json"))
 		.file(env, path.join(__dirname, env + ".json"))
 		.file('default', path.join(__dirname, "default.json"));
 	return nconf;		
