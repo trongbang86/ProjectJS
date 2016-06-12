83c090dd75af6706f86e139cef6a2a7ca3ff5a7c
trongbang86 <trongbang86@gmail.com>
2016-06-12 18:20:58 +1000

    fixed test-server/routes.test.js to use wrench to read file recursively

--- test/test-server/routes.test.js
+++ test/test-server/routes.test.js
@@ -1,8 +1,13 @@
 describe('Routes', function(){
 	it('loads and assigns routes from ' + 
 		'each definition file at their own base url', function(){
-		var fs = require('fs');
-		var defFiles = fs.readdirSync(TestProject.ROOT_FOLDER + '/server/routes');
-		expect(_.size(TestProject.routes)).to.eq(_.size(defFiles));
+		var wrench = require('wrench');
+		var path = require('path');
+		var defFiles = wrench.readdirSyncRecursive(TestProject.ROOT_FOLDER + '/server/routes');
+		expect(_.size(TestProject.routes))
+			.to.eq(_(defFiles).filter(function(file){
+				var ext = path.extname(file);
+				return ext === '.js';
+			}).length);
 	});
 });
